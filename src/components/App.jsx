654849e3009044d11fc32cedb1ab11740cb49e8e
Anastasiia-Onyshchuk <nastyaonyshchuk07@gmail.com>
2023-10-17 09:21:51 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import css from './app.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleAddContact = (newContact) => {
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleDeleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== contactId),
+    }));
+  };
+
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className={css.container}> 
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.handleAddContact} contacts={this.state.contacts} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} onFilterChange={this.handleFilterChange} />
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+// export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ContactItem } from './ContactListItem';
+import css from './contactlist.module.css';
+
+export const ContactList = ({ contacts, filter, onDeleteContact }) => {
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul>
+      {filteredContacts.map((contact) => (
+        <ContactItem key={contact.id} contact={contact} onDeleteContact={onDeleteContact} />
+      ))}
+    </ul>
+  );
+};
+
+// export default ContactList;
